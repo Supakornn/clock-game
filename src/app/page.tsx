7226eed0521e0db541527329557648f0ecf0d2f8
Supakorn <supakorn0728@gmail.com>
2024-11-14 22:15:53 +0700

    :sparkles: detect rectdevtool

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,24 @@
+"use client";
 // src/app/page.tsx - Server Component
 import Home from "./Home";
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
+  const [isDevToolsOpen, setIsDevToolsOpen] = useState(false);
+
+  useEffect(() => {
+    if (
+      (window as typeof window & { __REACT_DEVTOOLS_GLOBAL_HOOK__?: unknown })
+        .__REACT_DEVTOOLS_GLOBAL_HOOK__
+    ) {
+      console.log("React DevTools detected!");
+      setIsDevToolsOpen(true);
+    }
+  }, []);
+
+  if (isDevToolsOpen) {
+    return <div>React DevTools is open. Please close it to continue.</div>;
+  }
+
   return <Home />;
 }
