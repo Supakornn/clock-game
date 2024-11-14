76c98508c67d987bc872ab89671044e8f51ba9be
9hodev <siaskkh4884@gmail.com>
2024-11-14 19:49:39 +0700

    add rate-limit

--- src/pages/api/getTicketCode.ts
+++ src/pages/api/getTicketCode.ts
@@ -1,14 +1,25 @@
 // src/pages/api/getTicketCode.ts
 import { NextApiRequest, NextApiResponse } from 'next';
+import rateLimit from 'express-rate-limit';
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 2,
+  message: 'You look sus',
+  statusCode: 429,
+  keyGenerator: (req: NextApiRequest) => req.headers['user-agent'] || req.body.userId,
+});
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { timeElapsed } = req.body;
-  const targetTime = parseFloat(process.env.TARGET_TIME || '0');
-  const allowedRange = 0.5;
+  limiter(req, res, () => {
+    const { timeElapsed } = req.body;
+    const targetTime = parseFloat(process.env.TARGET_TIME || '0');
+    const allowedRange = 0.5;
 
-  if (Math.abs(timeElapsed - targetTime) < allowedRange) {
-    res.status(200).json({ success: true, ticketCode: process.env.TICKET_CODE });
-  } else {
-    res.status(403).json({ success: false, message: "Validation failed. Time does not match the target." });
-  }
+    if (Math.abs(timeElapsed - targetTime) < allowedRange) {
+      res.status(200).json({ success: true, ticketCode: process.env.TICKET_CODE });
+    } else {
+      res.status(403).json({ success: false, message: "Validation failed. Time does not match the target." });
+    }
+  });
 }
